f827e5e906a7de3c96c35790f89b9a5412aa45d7
Pier Costante Babini <116550787+piertv21@users.noreply.github.com>
2024-02-05 16:46:10 +0100

    Add first script

---
+++ main.py
@@ -0,0 +1,31 @@
+import os
+import PyPDF2
+import sys
+
+def count_pages_in_folder(folder):
+    total_pages = 0
+
+    if not os.path.exists(folder):
+        print(f"The folder '{folder}' does not exist.")
+        return
+
+    for filename in os.listdir(folder):
+        if filename.endswith(".pdf"):
+            file_path = os.path.join(folder, filename)
+
+            with open(file_path, "rb") as file:
+                pdf_reader = PyPDF2.PdfReader(file)
+                num_pages = len(pdf_reader.pages)
+
+                total_pages += num_pages
+
+                print(f"The file '{filename}' contains {num_pages} pages.")
+
+    print(f"\nTotal pages in all PDF files: {total_pages}")
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python script.py /path/to/your/folder")
+    else:
+        folder_to_analyze = sys.argv[1]
+        count_pages_in_folder(folder_to_analyze)
